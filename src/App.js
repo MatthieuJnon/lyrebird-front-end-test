d028687b3043272c564f6412417b979f2bd284cf
Matthieu Joannon <mattjoannon@gmail.com>
2018-11-20 15:19:03 +0100

    building first screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import "./style/main.scss";
+
+import Container from "react-bulma-components/lib/components/container";
+import Level from "react-bulma-components/lib/components/level";
+import Box from "react-bulma-components/lib/components/box";
+import LoadingBar from "./components/ProgressBar"
+import style from "./style/styles";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <React.StrictMode>
+          <Container breakpoint="desktop" style={style.paddingTop}>
+            <Box>
+              <Level renderAs="nav">
+                <Level.Item>Lyrebird Custom Voice</Level.Item>
+              </Level>
+            </Box>
+            <LoadingBar/>
+          </Container>
+        </React.StrictMode>
       </div>
     );
   }
